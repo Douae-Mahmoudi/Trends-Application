802d3eeaaaa7c2279c49fea1268f2d6060e4d0d7
saidi <saidimohammed0301@gmail.com>
2025-10-10 21:15:53 +0000

    modification en history et favoris

--- src/app/services/historique.service.ts
+++ src/app/services/historique.service.ts
@@ -12,11 +12,31 @@ export interface HistoriqueItem {
 
 @Injectable({ providedIn: 'root' })
 export class HistoriqueService {
-  private apiUrl = 'http://localhost:5000/api/history'; // ton Flask tourne sur ce port
+  private apiUrl = 'http://localhost:5000/api/history';
 
   constructor(private http: HttpClient) {}
 
   getTrends(): Observable<HistoriqueItem[]> {
-    return this.http.get<HistoriqueItem[]>(this.apiUrl);
+    return this.http.get<HistoriqueItem[]>(this.apiUrl, {
+      withCredentials: true  // ← AJOUTÉ ICI !
+    });
+  }
+
+  addToHistory(item: HistoriqueItem): Observable<any> {
+    return this.http.post(this.apiUrl, item, {
+      withCredentials: true  // ← AJOUTÉ ICI AUSSI !
+    });
+  }
+
+  clearHistory(): Observable<any> {
+    return this.http.delete(this.apiUrl, {
+      withCredentials: true  // ← AJOUTÉ ICI AUSSI !
+    });
+  }
+
+  deleteItem(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`, {
+      withCredentials: true  // ← AJOUTÉ ICI AUSSI !
+    });
   }
 }
